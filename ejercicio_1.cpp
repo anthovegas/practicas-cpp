2032670dbbcb6f54df564300d36a2b6cd83555ea
Anthonella Vegas <vegasvillalvaanthonella@gmail.com>
2025-10-27 15:39:47 -0500

    ejercicio...

--- ejercicio_1.cpp
+++ ejercicio_1.cpp
@@ -11,6 +11,7 @@
 //g++ ejercicio_1.cpp -o ejercicio_1
 //.\ejercicio_1.exe
 #include <iostream>
+#include <cstring>
 using namespace std;
 
 int buscar_num(int* matriz, int n){
@@ -20,9 +21,10 @@ int buscar_num(int* matriz, int n){
     return -1;
 }
 
-int buscar_letra(char* cadena, char* palabra){
-    for(int i=0; i<sizeof(cadena);++i){
-        if(cadena[i]==palabra[0]) return i;
+int buscar_letra(char* cadena, char letra){
+    int longitud = strlen(cadena);
+    for(int i=0; i<longitud;++i){
+        if(cadena[i]== letra) return i;
     }
     return -1;
 }
@@ -30,17 +32,22 @@ int buscar_letra(char* cadena, char* palabra){
 int main(){
     int matriz [10]={1,2,3,4,5,6,7,8,9,10};
     char palabra[100];
-    cout<<"Para datos numéricos: "<<endl;
-    cout<<"Ingrese el número a buscar: ";
+    cout<<"Para datos numericos: "<<endl;
+    cout<<"Ingrese el numero a buscar: ";
     int n;
     cin>>n;
+    cout<< buscar_num(matriz, n)<<endl;
 
-    cout<< buscar_num(matriz, n);
+    cin.ignore();
+
     cout<<"Para cadenas: "<<endl;
     cout<<"Ingrese la cadena: ";
     cin.getline(palabra, 100);
+
     cout<<"Ingrese la letra a buscar: ";
-    char letra[1];
-    cin.getline(letra, 1);
-    buscar_letra(palabra, letra);
+    char letra;
+    cin>>letra;
+
+    cout << buscar_letra(palabra, letra);
+    return 0;
 }
