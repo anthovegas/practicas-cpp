a365e09c88d64e6bb1c1086982e0d2e29b71b077
Anthonella Vegas <vegasvillalvaanthonella@gmail.com>
2025-12-22 19:47:43 -0500

    mfd

--- susti/p4.cpp
+++ susti/p4.cpp
@@ -9,14 +9,14 @@ struct Producto {
     int stock;
 };
 
-void registrarProducto (const char* nomArchivo) {
-    ofstream archivo(nomArchivo, ios::binary | ios::app);
+void Registrar(const char* nomArchivo) {
+    ofstream archivo(nomArchivo, ios::binary);
     if (!archivo) return;
 
     Producto p;
+    cout << "Ingrese los datos: " << endl;
+    cout << "Nombre: "; cin >> p.nombre;
     cout << "Codigo: "; cin >> p.codigo;
-    cin.ignore();
-    cout << "Nombre: "; cin.getline(p.nombre, 50);
     cout << "Precio: "; cin >> p.precio;
     cout << "Stock: "; cin >> p.stock;
 
@@ -24,23 +24,8 @@ void registrarProducto (const char* nomArchivo) {
     archivo.close();
 }
 
-void mostrarProductos (const char* nomArchivo) {
-    ifstream archivo(nomArchivo, ios::binary);
-    if(!archivo) {
-        cout << "Archivo no encontrado. " << endl;
-        return;
-    }
-
-    Producto p;
-    while (archivo.read((char*)&p, sizeof(Producto))) {
-        cout << "ID: " << p.codigo << " | " << p.nombre
-             << " | Precio: " << p.precio << " | Stock: " << p.stock << endl;
-    }
-    archivo.close();
-}
-
-void actualizarStock (const char* nomArchivo, int codBusq) {
-    fstream archivo (nomArchivo, ios::binary | ios::in | ios::out);
+void Actualizar (const char* nomArchivo, int codBusq) {
+    fstream archivo(nomArchivo, ios::binary);
     if (!archivo) return;
 
     Producto p;
@@ -50,7 +35,6 @@ void actualizarStock (const char* nomArchivo, int codBusq) {
         if (p.codigo == codBusq) {
             cout << "Nuevo stock para " << p.nombre << ": ";
             cin >> p.stock;
-
 
             archivo.seekp((int)archivo.tellg() - sizeof(Producto));
             archivo.write((char*)&p, sizeof(Producto));
@@ -63,6 +47,22 @@ void actualizarStock (const char* nomArchivo, int codBusq) {
     archivo.close();
 }
 
+void mostrarProducto(const char* nomArchivo) {
+    ifstream archivo (nomArchivo, ios::binary);
+    if(!archivo) {
+        cout << "Archivo no encontrado." << endl;
+        return;
+    }
+
+    Producto p;
+    while (archivo.read((char*)&p, sizeof(Producto))) {
+        cout << "ID: " << p.codigo << " | " << p.nombre
+             << " | Precio: " << p.precio << " | Stock: " << p.stock << endl;
+    }
+    archivo.close();
+}
+
+
 int main() {
     int op, cod;
     const char* inventario = "datos.bin";
@@ -71,13 +71,10 @@ int main() {
         cout << "\n1. Registrar\n2. Actualizar Stock\n3. Mostrar\n4. Salir\nOpcion: ";
         cin >> op;
         switch(op) {
-            case 1: registrarProducto(inventario); break;
-            case 2: 
-                cout << "Codigo a buscar: "; cin >> cod;
-                actualizarStock(inventario, cod); break;
-            case 3: mostrarProductos(inventario); break; 
+            case 1: Registrar(inventario); break;
+            case 2: cout << "Codigo a buscar: "; cin >> cod; Actualizar(inventario, cod); break;
+            case 3: mostrarProducto(inventario); break;
         }
     } while (op != 4);
-
     return 0;
 }
