653afd7880028d04c944b3f9e5407f7c2faccac3
Anthonella Vegas <vegasvillalvaanthonella@gmail.com>
2026-05-03 16:41:11 -0500

    matriz potencia

---
+++ 3er ciclo/planchas pc2/matrizpotencia.cpp
@@ -0,0 +1,78 @@
+#include "matrizpotencia.h"
+#include <iomanip>
+#include <iostream>
+using namespace std;
+
+void CreateMatrix(TP **&pMat, const size_t rows, const size_t cols) {
+    pMat = new TP *[rows];
+    for (size_t i = 0; i < rows; ++i) 
+        pMat[i] = new TP [cols];
+}
+
+void ReadMatrix(TP **pMat, const size_t rows, const size_t cols) {
+    cout << "Ingrese los valores: " << endl;
+    for (size_t i = 0; i < rows; ++i) {
+        for (size_t j = 0; j < cols; ++j) {
+            cin >> pMat[i][j];
+        } 
+    }
+}
+
+void PrintMatrix(TP **pMat, const size_t rows, const size_t cols) {
+    for (size_t i = 0; i < rows; ++i) {
+        for (size_t j = 0; j < cols; ++j) {
+            cout << pMat[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+void DeleteMatrix(TP **&pMat, const size_t rows) {
+    for (size_t i = 0; i < rows; ++i) 
+        delete[] pMat[i];
+    delete[] pMat;
+    pMat = nullptr;
+}
+
+void multiplicarMatrices(TP **pMatA, TP **pMatB, TP **&pMatC, int n) {
+    for (auto i = 0; i < n; ++i) {
+        for (auto j = 0; j < n; ++j) {
+            pMatC[i][j] = 0;
+            for (auto k = 0; k < n; ++k) {
+                pMatC[i][j] += pMatA[i][k] * pMatB[k][j];
+            }
+        }
+    }
+}
+
+void matrizPotencia(TP **&A, int k, int tamaño) {
+    if (k == 0) {
+        for (auto i = 0; i < tamaño; ++i) {
+            for (auto j = 0; j < tamaño; ++j) {
+                if (i == j) {
+                    A[i][j] = 1;
+                } else {
+                    A[i][j] = 0;
+                }
+            }
+        }
+    }
+
+    if (k < 0 || k == 1) return;
+
+    TP **Resultado, **Base;
+    CreateMatrix(Resultado, tamaño, tamaño);
+    CreateMatrix(Base, tamaño, tamaño);
+
+    for(auto i = 0; i < tamaño; ++i)
+        for (auto j = 0; j < tamaño; ++j)
+            Base[i][j] = A[i][j];
+    
+    for (auto i = 1; i < k; ++i) {
+        multiplicarMatrices(A, Base, Resultado, tamaño);
+
+        for (auto i = 0; i < tamaño; ++i)
+            for (auto j = 0; j < tamaño; ++j)
+                A[i][j] = Resultado[i][j];
+    }
+}
---
+++ 3er ciclo/planchas pc2/matrizpotencia.h
@@ -0,0 +1,15 @@
+#ifndef __MATRIZPOTENCIA_H__
+#define __MATRIZPOTENCIA_H__
+
+using TP = unsigned long long int;
+using size_t = int;
+
+void CreateMatrix(TP **&pMat, const size_t rows, const size_t cols);
+void ReadMatrix(TP **pMat, const size_t rows, const size_t cols);
+void PrintMatrix(TP **pMat, const size_t rows, const size_t cols);
+void DeleteMatrix(TP **&pMat, const size_t rows);
+
+void multiplicarMatrices(TP **pMatA, TP **pMatB, TP **&pMatC, int n);
+void matrizPotencia(TP **&A, int k, int tamaño);
+
+#endif // __MATRIZPOTENCIA_H__
